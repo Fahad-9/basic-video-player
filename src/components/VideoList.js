5c0813700c9b481f6a22789559d0b4e3b7cfcb76
Fahad Rahman <kfarm9125@gmail.com>
2020-09-15 18:39:23 +0530

    commit-video-player

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,11 +5,12 @@ import { Grid } from '@material-ui/core';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
-    const listOfVideos = videos.map((video, id) => <VideoItem onVideoSelect={onVideoSelect} key={id} video={video}/>) 
+    const listOfVideos = videos.map((video, id) => <VideoItem onVideoSelect={onVideoSelect} key={id} video={video} />)
     return (
-        <Grid container spacing={10}>
+        <Grid container direction="column" justify="flex-start" alignItems="flex-end">
             {listOfVideos}
         </Grid>
+
     )
 }
 
